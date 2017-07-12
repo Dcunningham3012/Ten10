44f8007e6e3b316aac637c393683290880e77c9d
David Cunningham <dcunningham@itestonline.co.uk>
2017-07-12 10:48:26 +0100

    commit

--- src/test/java/WebServiceTest2.java
+++ src/test/java/WebServiceTest2.java
@@ -1,15 +1,15 @@
-import org.junit.Test;
-
-import javax.xml.soap.SOAPConnection;
-import javax.xml.soap.SOAPConnectionFactory;
-import javax.xml.soap.SOAPMessage;
-
-public class WebServiceTest2 extends Functions {
-    @Test
-    public void WS1() throws Exception {
-        SOAPMessage message = createSOAPRequest("C:\\Users\\Work PC\\Desktop\\EssexLocation.xml");
-        SOAPMessage response = getSOAPResponse(message, "http://www.webservicex.net/uklocation.asmx" );
-        validateValue(response, "County", "Essex" );
-        System.out.print(response);
-    }
-}
+//import org.junit.Test;
+//
+//import javax.xml.soap.SOAPConnection;
+//import javax.xml.soap.SOAPConnectionFactory;
+//import javax.xml.soap.SOAPMessage;
+//
+//public class WebServiceTest2 extends Functions {
+//    @Test
+//    public void WS1() throws Exception {
+//        SOAPMessage message = createSOAPRequest("C:\\Users\\Work PC\\Desktop\\EssexLocation.xml");
+//        SOAPMessage response = getSOAPResponse(message, "http://www.webservicex.net/uklocation.asmx" );
+//        validateValue(response, "County", "Essex" );
+//        System.out.print(response);
+//    }
+//}
